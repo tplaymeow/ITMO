424515a4a1877f9c4e1f331e4e8476f4232a44d2
Timur Guliamov <wersa2002@gmail.com>
2020-10-26 01:42:01 +0300

    fix second task

--- Course 1/infa/inf_lab4/second_task.py
+++ Course 1/infa/inf_lab4/second_task.py
@@ -6,7 +6,7 @@ strings = file.readlines()
 pattern = r'\b(?:[bcdfghjklmnpqrstvwxz]*[aeiouy][bcdfghjklmnpqrstvwxz]*){2}\b'
 
 for i in strings:
-    if (len(i.strip().split(' ')) == 6) and (len(re.findall(pattern, i.lower())) == 4):
+    if (len(i.strip().split(' ')) == 6) and (len(re.findall(pattern, i.lower())) == 1):
         print(i, end="")
 
 file.close()
